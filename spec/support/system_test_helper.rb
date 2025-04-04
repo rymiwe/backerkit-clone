6c109c4251bfdba3470823d599c69bbc6e9a2da1
rymiwe <rymiwe@gmail.com>
2025-04-04 16:09:08 -0700

    Enhance system test configuration for more stable CI runs

--- spec/support/system_test_helper.rb
+++ spec/support/system_test_helper.rb
@@ -7,6 +7,17 @@ Webdrivers::Chromedriver.required_version = '114.0.5735.90' # Using a known stab
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
+    # Set Chrome options for a more stable CI environment
+    options = Selenium::WebDriver::Chrome::Options.new
+    
+    # Add more stability options for CI
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1400,1400')
+    
+    # Use the configured Chrome with these options
+    driven_by :selenium, using: :chrome, options: options
   end
 end
