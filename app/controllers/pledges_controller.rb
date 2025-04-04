25372a1a9a01c6285b4b66ea5b90018b71985581
rymiwe <rymiwe@gmail.com>
2025-04-04 11:47:09 -0700

    Fix pledging with no reward: Handle nil reward_id properly to prevent foreign key violations

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -11,6 +11,9 @@ class PledgesController < ApplicationController
   
   # POST /projects/:project_id/pledges
   def create
+    # Clean up the params to handle "no reward" case properly
+    clean_reward_params
+    
     @pledge = build_pledge(pledge_params)
     
     if @pledge.save
@@ -37,6 +40,13 @@ class PledgesController < ApplicationController
   
   def pledge_params
     params.require(:pledge).permit(:amount, :reward_id)
+  end
+  
+  # Handle case where reward_id is submitted as empty string or "0"
+  def clean_reward_params
+    if params[:pledge] && (params[:pledge][:reward_id].blank? || params[:pledge][:reward_id] == "0")
+      params[:pledge][:reward_id] = nil
+    end
   end
   
   def build_pledge(attributes = {})
