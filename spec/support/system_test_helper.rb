0e5e05c9b2ebe89f63d1c236a7b7b83480c58f83
rymiwe <rymiwe@gmail.com>
2025-04-04 15:53:37 -0700

    Fix: Specify compatible chromedriver version for CI

--- spec/support/system_test_helper.rb
+++ spec/support/system_test_helper.rb
@@ -1,4 +1,9 @@
 require 'capybara/rspec'
+require 'webdrivers/chromedriver'
+
+# Configure Webdrivers to use a specific chromedriver version that's compatible
+# with Semaphore CI's environment
+Webdrivers::Chromedriver.required_version = '114.0.5735.90' # Using a known stable version
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
